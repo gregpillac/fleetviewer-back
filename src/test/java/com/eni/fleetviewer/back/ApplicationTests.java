0d7af7d8b3df715e3b6d20a8b9d1ea40cf344168
Grepiac <gregory.pillac@gmail.com>
2025-03-26 15:33:30 +0100

    Spring Initializer

---
+++ src/test/java/com/eni/fleetviewer/back/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eni.fleetviewer.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
