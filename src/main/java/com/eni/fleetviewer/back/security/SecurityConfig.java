fa39767d06685548e6f646a1193a533a269c5a81
Grepiac <gregory.pillac@gmail.com>
2025-05-02 09:42:52 +0200

    Service JWT

--- src/main/java/com/eni/fleetviewer/back/security/SecurityConfig.java
+++ src/main/java/com/eni/fleetviewer/back/security/SecurityConfig.java
@@ -2,26 +2,37 @@ package com.eni.fleetviewer.back.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable) // Désactiver CSRF
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("api/public/**").permitAll()
-                        .anyRequest().authenticated()
+                // Désactiver CSRF (ok pour une API REST)
+                .csrf(AbstractHttpConfigurer::disable)
+                // Pas de session (API REST stateless on utilise un token JWT)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .httpBasic(Customizer.withDefaults())  // Authentification HTTP Basic
-                .formLogin(Customizer.withDefaults()); // Login formulaire
+                // Règles d'autorisation
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/public/**").permitAll()
+                        .anyRequest().authenticated()
+                );
 
         return http.build();
     }
