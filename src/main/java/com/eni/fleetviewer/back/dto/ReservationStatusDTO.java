81fc80fb13bca43f855b08c46379ae883c8c6188
ngoclinh <linh.tran190214@gmail.com>
2025-09-01 12:26:28 +0200

    [RES_STATUS] : Get ReservationStatus + test Unitaire  + test Postman 200  mais j'ai pas de données dans la bdd

---
+++ src/main/java/com/eni/fleetviewer/back/dto/ReservationStatusDTO.java
@@ -0,0 +1,12 @@
+package com.eni.fleetviewer.back.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReservationStatusDTO {
+    private Long id;
+    private String name;
+}
