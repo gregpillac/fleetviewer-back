3a81f48ee6b08bcf32eaac8a6bda116531b2153f
Grepiac <gregory.pillac@gmail.com>
2025-09-09 22:00:28 +0200

    [RESA] : Recuperation des reservations compatibles (sans controle du nb de places)

--- src/main/java/com/eni/fleetviewer/back/repository/ReservationRepository.java
+++ src/main/java/com/eni/fleetviewer/back/repository/ReservationRepository.java
@@ -1,5 +1,7 @@
 package com.eni.fleetviewer.back.repository;
 
+import com.eni.fleetviewer.back.dto.ReservationDTO;
+import com.eni.fleetviewer.back.model.Place;
 import com.eni.fleetviewer.back.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -20,4 +22,29 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
             "AND r.startDate < :endDate AND r.endDate > :startDate")
     List<Reservation> findByVehicleIdAndDates(@Param("vehicleId") Long vehicleId, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 
+
+    /**
+     * Réservations compatibles en considérant :
+     *  - soit (depart cherché = depart existant ET date de départ le jour de depart existant),
+     *  - soit (depart cherché = destination existante ET date de départ le jour de retour existant).
+     *
+     */
+    @Query("""
+       SELECT r FROM Reservation r
+       WHERE (
+              (r.departure.id = :requestedDeparturePlaceId
+               AND r.startDate >= :departureStartOfDay
+               AND r.startDate <  :departureEndOfDay)
+           OR (r.arrival.id = :requestedDeparturePlaceId
+               AND r.endDate >= :departureStartOfDay
+               AND r.endDate <  :departureEndOfDay)
+         )
+       """)
+    List<Reservation> findCompatibleReservationsOnStartDateAndPlace(
+            @Param("requestedDeparturePlaceId") Long departurePlaceId,
+            @Param("departureStartOfDay") LocalDateTime departureStartOfDay,
+            @Param("departureEndOfDay")   LocalDateTime departureEndOfDay);
+
+    //TOD0: à implémenter Apres ajout du nb de places restantes dans la Réservation
+    //List<ReservationDTO> findCompatibleReservationsOnStartDateAndPlaceAndSeatsLeft();
 }
